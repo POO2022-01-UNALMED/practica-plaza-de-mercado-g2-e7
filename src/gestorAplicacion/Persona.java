f4867f25c02e84a6c92d606f7d013d83961d5be0
dapuentes <dapuentes@unal.edu.co>
2022-05-30 02:31:17 -0500

    Cambios menores en Persona

--- src/gestorAplicacion/Persona.java
+++ src/gestorAplicacion/Persona.java
@@ -2,9 +2,9 @@ package gestorAplicacion;
 
 /**
  * Clase: Persona
- * Esta clase es abstracta, ya que no es de interes crear instancias de ella
- * Se utilizara para que las clases Duenho, Cliente y Codeudor hereden sus atributos
- * E implementen los métodos obtenerDatos() y retornarInformacion()
+ * Esta clase es abstracta, ya que no es de interés crear instancias de ella.
+ * Se utilizará para que las clases Duenho, Cliente y Codeudor hereden sus atributos
+ * e implementen los métodos obtenerDatos() y retornarInformacion()
  * @author: Daniel Puentes
  */
 
@@ -42,32 +42,32 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna la informacion completa de la clase, que puede ser utilizada
-     * para crear reportes y por ende listar las personas o simplemente ver la informacion
+     * Este método retorna la información completa de la clase, que puede ser utilizada
+     * para crear reportes y por ende listar las personas o simplemente ver la información
      * de una persona
-     * @return La informacion completa de la clase
+     * @return La información completa de la clase
      */
     public abstract String retornarInformacion();
 
     /**
-     * Este metodo retorna la cedula y el nombre de la persona, y se implemento para
-     * que al momento de listar las personas se diera una informacion resumida
-     * @return La cedula y el nombre de la persona
+     * Este método retorna la cédula y el nombre de la persona, y se implementó para
+     * que al momento de listar las personas se diera una información resumida
+     * @return La cédula y el nombre de la persona
      */
     protected String retornarInformacionCorta() {
         return "Cedula: " + cedula + "\nNombre: " + nombre;
     }
 
     /**
-     * Este metodo retorna la cedula de la persona
-     * @return Cedula de la persona
+     * Este método retorna la cédula de la persona
+     * @return Cédula de la persona
      */
     public int getCedula() {
         return this.cedula;
     }
 
     /**
-     * Este metodo asigna la cedula a la persona
+     * Este método asigna la cédula a la persona
      * @param cedula a asignar
      */
     public void setCedula(int cedula) {
@@ -75,7 +75,7 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna el nombre de la persona
+     * Este método retorna el nombre de la persona
      * @return Nombre de la persona
      */
     public String getNombre() {
@@ -83,7 +83,7 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo asigna el nombre a la persona
+     * Este método asigna el nombre a la persona
      * @param nombre a asignar
      */
     public void setNombre(String nombre) {
@@ -91,15 +91,15 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna el telefono de la persona
-     * @return Telefono de la persona
+     * Este método retorna el teléfono de la persona
+     * @return Teléfono de la persona
      */
     public int getTelefono() {
         return this.telefono;
     }
 
     /**
-     * Este metodo asigna el telefono a la persona
+     * Este método asigna el télefono a la persona
      * @param telefono a asignar
      */
     public void setTelefono(int telefono) {
@@ -107,15 +107,15 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna la direccion de la persona
-     * @return Direccion de la persona
+     * Este método retorna la dirección de la persona
+     * @return Dirección de la persona
      */
     public String getDireccion() {
         return this.direccion;
     }
 
     /**
-     * Este metodo asigna la direccion a la persona
+     * Este método asigna la dirección a la persona
      * @param direccion a asignar
      */
     public void setDireccion(String direccion) {
@@ -123,15 +123,15 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna el genero de la persona
-     * @return Genero de la persona
+     * Este método retorna el género de la persona
+     * @return Género de la persona
      */
     public char getGenero() {
         return this.genero;
     }
 
     /**
-     * Este metodo asigna el genero a la persona
+     * Este método asigna el género a la persona
      * @param genero a asignar
      */
     public void setGenero(char genero) {
@@ -139,7 +139,7 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo retorna el estado civil de la persona
+     * Este método retorna el estado civil de la persona
      * @return Estado civil de la persona
      */
     public String getEstadoCivil() {
@@ -147,7 +147,7 @@ public abstract class Persona {
     }
 
     /**
-     * Este metodo asigna el estado civil a la persona
+     * Este método asigna el estado civil a la persona
      * @param estadoCivil a asignar
      */
     public void setEstadoCivil(String estadoCivil) {
